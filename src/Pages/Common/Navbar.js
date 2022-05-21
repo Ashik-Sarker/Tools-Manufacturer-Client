7d83abcfad8616e592ec615d23e84cdab9ad2a70
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-05-22 03:51:34 +0600

    navbar and banner don

--- src/Pages/Common/Navbar.js
+++ src/Pages/Common/Navbar.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const menuItem =
+    <>
+        <li><Link className='text-green-700 font-bold' to='/tools'>Tools</Link></li>
+        <li><Link className='text-green-700 font-bold' to='/businessSummary'>Business Summary</Link></li>
+        <li><Link className='text-green-700 font-bold' to='/reviews'>Reviews</Link></li>
+        <li><Link className='text-green-700 font-bold' to='/login'>Login</Link></li>
+        <li><Link className='text-green-700 font-bold' to='/registration'>Registration</Link></li>
+        <li><Link className='text-green-700 font-bold' to='/logout'>Logout</Link></li>
+    </>
     return (
-        <div>
-            <h1>Navbar</h1>
+        <div class="navbar bg-base-100 sticky top-0 z-50">
+            <div class="navbar-start">
+                <div class="dropdown">
+                <label tabindex="0" class="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
+                <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    {menuItem}
+                </ul>
+                </div>
+                <Link to='/' class="btn btn-ghost normal-case text-xl text-green-700 font-bold">BTM</Link>
+            </div>
+            <div class="navbar-end hidden lg:flex">
+                <ul class="menu menu-horizontal p-0">
+                    {menuItem}
+                </ul>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,9 +1,48 @@
 import React from 'react';
-
+import banner1 from '../../Assets/Banner/banner1.jpg'
+import banner2 from '../../Assets/Banner/banner2.jpg'
+import banner3 from '../../Assets/Banner/banner3.jpg'
+import banner4 from '../../Assets/Banner/banner-3.jpg'
+import bigBanner1 from '../../Assets/Banner/bigBanner1.jpg'
+import bigBanner2 from '../../Assets/Banner/bigBanner2.jpg'
+import bigBanner3 from '../../Assets/Banner/bigBanner3.jpg'
+import bigBanner4 from '../../Assets/Banner/bigBanner4.jpg'
+import bigBanner5 from '../../Assets/Banner/bigBanner5.jpg'
+import banner9 from '../../Assets/Banner/banner9.jpg'
+import banner8 from '../../Assets/Banner/banner8.jpg'
+import banner6 from '../../Assets/Banner/banner6.jpg'
+import banner5 from '../../Assets/Banner/banner5.jpg'
 const Banner = () => {
     return (
-        <div>
-            <h1>Banner</h1>
+        <div class="carousel slide w-full">
+            <div id="slide1" class="carousel-item relative w-full">
+                <img src={banner9} class="w-full" alt='' /> / 
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide4" class="btn btn-circle">❮</a> 
+                <a href="#slide2" class="btn btn-circle">❯</a>
+                </div>
+            </div> 
+            <div id="slide2" class="carousel-item relative w-full">
+                <img src={banner8} class="w-full" alt=''/> / 
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide1" class="btn btn-circle">❮</a> 
+                <a href="#slide3" class="btn btn-circle">❯</a>
+                </div>
+            </div> 
+            <div id="slide3" class="carousel-item relative w-full">
+                <img src={banner6} class="w-full" alt=''/> / 
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide2" class="btn btn-circle">❮</a> 
+                <a href="#slide4" class="btn btn-circle">❯</a>
+                </div>
+            </div> 
+            <div id="slide4" class="carousel-item relative w-full">
+                <img src={banner5} class="w-full" alt=''/> / 
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide3" class="btn btn-circle">❮</a> 
+                <a href="#slide1" class="btn btn-circle">❯</a>
+                </div>
+            </div>
         </div>
     );
 };
